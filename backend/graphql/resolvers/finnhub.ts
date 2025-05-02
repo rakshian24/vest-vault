95541986a1aebb1c2ff10a05cbab34a2f2d819f7
rakshian24 <rakshithrao123@gmail.com>
2025-05-02 16:26:16 +0530

    feat: removed node cache & added redis cache

--- backend/graphql/resolvers/finnhub.ts
+++ backend/graphql/resolvers/finnhub.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { cache } from "../../utils/cache";
+import { redis } from "../../utils/redis";
 import getLoggedInUserId from "../../middleware/getLoggedInUserId";
 import { ApolloError } from "apollo-server-errors";
 
@@ -17,31 +17,37 @@ const resolvers = {
     ): Promise<IStockPrice> => {
       const loggedInUserId = getLoggedInUserId(ctx);
       const userId = loggedInUserId?.userId;
-
       if (!userId) {
         throw new ApolloError("User not authenticated", "NOT_AUTHENTICATED");
       }
 
       const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
 
-      const stockKey = `stock:${symbol}`;
+      if (!FINNHUB_API_KEY) {
+        throw new Error("Missing FINNHUB_API_KEY");
+      }
 
+      const stockKey = `stock:${symbol}`;
       let source: "api" | "cache" = "api";
+
       let currentPrice: number;
 
-      const cachedStock = cache.get<number>(stockKey);
+      const cachedPrice = await redis.get<number>(stockKey);
 
-      if (cachedStock !== undefined) {
-        currentPrice = cachedStock;
+      if (cachedPrice !== null && cachedPrice > 0) {
+        currentPrice = cachedPrice;
         source = "cache";
       } else {
         const response = await axios.get(
           `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${FINNHUB_API_KEY}`
         );
+        const fetchedPrice = response.data?.c ?? 0;
 
-        currentPrice = response.data?.c ?? 0;
+        currentPrice = fetchedPrice;
 
-        if (currentPrice > 0) cache.set(stockKey, currentPrice);
+        if (currentPrice > 0) {
+          await redis.set(stockKey, currentPrice, { ex: 180 }); // 3 minutes TTL
+        }
       }
 
       return {
